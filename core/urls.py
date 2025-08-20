6c6587b52f4743cc7722e150dc4f303287079243
Tamim <tamimvaiya.123@gmail.com>
2025-08-20 18:44:39 +0600

    Added Detail View and Desinged It

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('product/<int:id>/', detail, name='detail'),
 ]
