37623949c5fb9a80384dc04e5b132475bbb29367
Tamim <tamimvaiya.123@gmail.com>
2025-08-20 17:35:03 +0600

    Displaying Products

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.all()
+    return render(request, 'core/index.html', {'products': products})
