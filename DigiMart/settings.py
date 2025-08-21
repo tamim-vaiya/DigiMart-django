c7e2f8246909214b6ccd5d5da2ed8caaf1f1ddfb
Tamim <tamimvaiya.123@gmail.com>
2025-08-21 20:42:00 +0600

    Created a Stripe checkout session

--- DigiMart/settings.py
+++ DigiMart/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -122,3 +127,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Product
+from django.conf import settings
+import stripe, json
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -8,4 +12,31 @@ def index(request):
 
 def detail(request, id):
     product = Product.objects.get(id=id)
-    return render(request, 'core/detail.html', {'product':product})
+    stripe_publishable_key = settings.STRIPE_PUBLISHABLE_KEY
+    return render(request, 'core/detail.html', {'product':product, 'stripe_publishable_key' : stripe_publishable_key})
+
+@csrf_exempt
+def create_checkout_session(request, id):
+    request_data = json.load(request.body)
+    product = Product.objects.get(id=id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        customer_email = request_data['email'],
+        payment_method_types= ['card'],
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': product.name,
+                    },
+                    'unit_amount': int(product.price * 100)
+                },
+                'quantity': 1,
+            }
+        ],
+        mode='payment',
+        success_url= request.build_absolute_uri(reverse('success')) +
+        "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url= request.build_absolute_uri(reverse('failed')),
+    )
