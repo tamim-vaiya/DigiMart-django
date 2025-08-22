6edfd1ea055b6fba18b139941b08f60493daebf9
Tamim <tamimvaiya.123@gmail.com>
2025-08-22 20:02:57 +0600

    Added Payment Success View

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, detail
+from .views import index, detail, payment_success_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('product/<int:id>/', detail, name='detail'),
+    path('success/', payment_success_view, name='success'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, OrderDetail
 from django.conf import settings
 import stripe, json
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 
 # Create your views here.
 def index(request):
@@ -50,3 +50,15 @@ def create_checkout_session(request, id):
     order.save()
 
     return JsonResponse({'session_id':checkout_session.id})
+
+def payment_success_view(request):
+    session_id = request.GET.get('session_id')
+    if session_id is None:
+        return HttpResponseNotFound()
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.retrieve(session_id)
+    order = get_object_or_404(OrderDetail, stripe_payment_intent = session.pyament_intent)
+    order.has_paid = True
+    order.save()
+
+    return render(request, 'core/payment_success.html', {'order': order})
