593ded48adbe8ea9efcb999837a300e9734ff326
Tamim <tamimvaiya.123@gmail.com>
2025-08-23 18:43:44 +0600

    URL for Create Checkout View

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, detail, payment_success_view, payment_failed_view
+from .views import index, detail, payment_success_view, payment_failed_view, create_checkout_session
 
 urlpatterns = [
     path('', index, name='index'),
     path('product/<int:id>/', detail, name='detail'),
     path('success/', payment_success_view, name='success'),
     path('failed/', payment_failed_view, name='failed'),
+    path('api/checkout-session/<int:id>/', create_checkout_session, name='api_checkout_session'),
 ]
