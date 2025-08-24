65cbb34881b8974b42b4754efaab160326f190aa
Tamim <tamimvaiya.123@gmail.com>
2025-08-24 16:21:09 +0600

    Fixed Errors

--- core/views.py
+++ core/views.py
@@ -18,7 +18,7 @@ def detail(request, id):
 
 @csrf_exempt
 def create_checkout_session(request, id):
-    request_data = json.load(request.body)
+    request_data = json.loads(request.body)
     product = Product.objects.get(id=id)
     stripe.api_key = settings.STRIPE_SECRET_KEY
     checkout_session = stripe.checkout.Session.create(
@@ -45,7 +45,8 @@ def create_checkout_session(request, id):
     order = OrderDetail()
     order.customer_email = request_data['email']
     order.product = product
-    order.stripe_payment_intent = checkout_session['payment_intent']
+    # order.stripe_payment_intent = checkout_session['payment_intent']
+    order.stripe_session_id=checkout_session.id
     order.amount = int(product.price)
     order.save()
 
@@ -57,8 +58,9 @@ def payment_success_view(request):
         return HttpResponseNotFound()
     stripe.api_key = settings.STRIPE_SECRET_KEY
     session = stripe.checkout.Session.retrieve(session_id)
-    order = get_object_or_404(OrderDetail, stripe_payment_intent = session.pyament_intent)
+    order = get_object_or_404(OrderDetail, stripe_session_id=session.id)
     order.has_paid = True
+    order.stripe_payment_intent = session.payment_intent
     order.save()
 
     return render(request, 'core/payment_success.html', {'order': order})
