34043c0ed6fae1c0542a0475bb74e93f8b70cf10
Tamim <tamimvaiya.123@gmail.com>
2025-08-28 08:50:40 +0600

    Styled the Registration page

--- core/forms.py
+++ core/forms.py
@@ -8,15 +8,37 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'description', 'price', 'file']
         
 class UserRagistrationForm(forms.ModelForm):
-    password = forms.CharField(label='password' , widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password' , widget=forms.PasswordInput)
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+        })
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+        })
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            }),
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
+            raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
         
