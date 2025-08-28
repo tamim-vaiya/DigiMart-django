db02a1ad0605a54be449084f15aadaf0574930b7
Tamim <tamimvaiya.123@gmail.com>
2025-08-28 08:44:12 +0600

    Handled POST Request

--- core/views.py
+++ core/views.py
@@ -104,5 +104,12 @@ def dashboard(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        user_form = UserRagistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return redirect('index')
     user_form = UserRagistrationForm()
     return render(request, 'core/register.html', {'user_form': user_form})
